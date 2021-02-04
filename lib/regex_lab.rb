6fbc82387189853d5631a394fd7775a10eb0583c
UGUR GUNDOGDU <ugurgundogdu@me.com>
2021-02-04 21:00:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-
+    if word.match(/^[aeiouAEIOU]\w+/)
+       return true
+     else
+       false
+     end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
