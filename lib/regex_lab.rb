d19ecea3795924c244c41f15b41d94e04d5fb75f
UGUR GUNDOGDU <ugurgundogdu@me.com>
2021-02-04 21:16:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-   words.scan(/^\w{5}\s/)
+   word.scan(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
