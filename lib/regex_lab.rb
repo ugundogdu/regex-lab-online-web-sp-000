0f6ac258f142d8d365caf572a5affa246cd073ce
UGUR GUNDOGDU <ugurgundogdu@me.com>
2021-02-04 22:00:25 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-
+    if word.match(/^[aeiouAEIOU]\w+/)
+       return true
+     else
+       false
+     end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+   text.scan(/\b\w[a-z]{4}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !!text.match(/^[A-Z].+\W$/)
 end
 
 def valid_phone_number?(phone)
-
+    phone.scan(/\d/).count == 10
 end
